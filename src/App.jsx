e6b267ce9d9c19b1896df762226303a4a8939d00
yutarohabu <hup0628@gmail.com>
2025-03-14 17:54:00 +0900

    react適応完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import React from 'react';
 import styles from './App.module.css'
+import {Test} from './test.jsx'
 
 export const App = ()=>{
   return(
-    <Top />
+    <>
+      <Top />
+      <Test />
+      <div>aaa</div>
+    </>
   )
 }
 
 const Top = ()=>{
+  console.log(`トップコンポーネント`)
   return(
-    <h2>Top</h2>
+    <h2 className={styles.title}>Top</h2>
   )
 }
